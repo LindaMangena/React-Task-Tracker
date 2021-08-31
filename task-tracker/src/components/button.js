375d279e9a98b0da94b05a63c030d2a3168bd36e
lindajazz42@gmail.com <lindajazz42@gmail.com>
2021-08-31 16:04:31 +0200

    click event

--- task-tracker/src/components/button.js
+++ task-tracker/src/components/button.js
@@ -1,8 +1,26 @@
+import PropTypes from 'prop-types'
 
 const Button = ({ color, text }) => {
-    return (<button style={{ backgroundColor: color }} className='btn'>{text}</button>
+
+    return (
+        <button onClick = {onclick} style=
+            {{ backgroundColor: color }}
+            className='btn'>
+            {text}
+        </button>
 
     )
+}
+
+Button.defaultProps = {
+    color: 'Steelblue'
+
+}
+
+Button.propTypes = {
+
+    text: PropTypes.string,
+    color: PropTypes.string
 }
 
 export default Button
