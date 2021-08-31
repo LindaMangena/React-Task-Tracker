a6c0b85cc52de97bd42561bd3f8c1055bf4098fa
lindajazz42@gmail.com <lindajazz42@gmail.com>
2021-08-31 16:50:55 +0200

    Mock Tasks

--- task-tracker/src/components/Tasks.js
+++ task-tracker/src/components/Tasks.js
@@ -1,3 +1,25 @@
+
+const Tasks = [{
+    id : 1,
+    text: 'Doctors Appointment',
+    day: 'Feb 5th at 2:30',
+    reminder : true,
+},
+{
+    id : 2,
+    text: 'Meeting at school',
+    day: 'Feb 5th at 1:30',
+    reminder : true,
+},
+{
+
+    id : 3,
+    text: 'Food shopping ',
+    day: 'Feb 5th at 2:30',
+    reminder : false,
+}
+]
+
 const Tasks = () => {
     return (
         <div>
