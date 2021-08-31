8dd179b7ed7abaa14b3040ec025f6ee8e02f5f2d
academy_intern_mac05 <academy_intern_mac05@academy-intern-mac05s-MacBook-Air.local>
2021-08-31 11:58:49 +0200

    changes

--- task-tracker/src/components/button.js
+++ task-tracker/src/components/button.js
@@ -1,7 +1,7 @@
 
-const Button = ({color,text}) => {
-    return ( <button style={{backgroundColor: color}} className='btn'>{text}</button>
-        
+const Button = ({ color, text }) => {
+    return (<button style={{ backgroundColor: color }} className='btn'>{text}</button>
+
     )
 }
 
