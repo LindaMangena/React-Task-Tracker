10a7c5e98401b84f71d76bdda5c90a101f032888
lindajazz42@gmail.com <lindajazz42@gmail.com>
2021-09-02 12:14:16 +0200

    delete button

--- task-tracker/src/components/Task.js
+++ task-tracker/src/components/Task.js
@@ -1,9 +1,10 @@
-const Task = ({task}) => {
+import { FaTimes } from 'react-icons/fa'
+const Task = ({ task }) => {
     return (
         <div className='task'>
-        <h3>{task.text}</h3>
-        <p>{task.day}</p>
-            
+            <h3>{task.text}<FaTimes style={{color: 'red', cursor: 'pointer'}}/> </h3>
+            <p>{task.day}</p>
+
         </div>
     )
 }
