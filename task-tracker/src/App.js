6ee19493d4c2e9a590a849123847d443ecf955ef
LindaMangena <lindajazz42@gmail.com>
2021-09-02 22:36:09 -0400

    deleteMethod

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -23,11 +23,17 @@ function App(){
     reminder: false,
 }
 ])
+
+// Delete Task 
+const deleteTask =(id) => {
+  console.log('delete',id);
+
+}
   return (
     <div className='container'> 
 
      <Header />
-     <Tasks tasks={tasks} />
+     <Tasks tasks={tasks} onDelete={deleteTask}/>
     </div>
   )
 }
